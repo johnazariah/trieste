746bc6a1ecd2895c18682ffade8356c33849c33a
John Azariah <john.azariah@gmail.com>
2016-11-11 08:37:00 -0800

    Show graph for selected Cluster/Run

--- shell/app/server-proxy/monitor-proxy.ts
+++ shell/app/server-proxy/monitor-proxy.ts
@@ -1,14 +1,25 @@
 export default class MonitorProxy {
-    public load(cluster_id, run_id) {        
+    public load(cluster_id, run_id, do_something_with_json) {
         const exec = require('child_process').exec;
 
-        let cmd = `cd .. && python cntklogparser.py $cluster_id\\$run_id\\stderr.txt`
-        exec(cmd, (err, stdout, stderr) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log(stdout);
+        var result : any;
+        
+        let downloadLogCommand = `cd .. && python trieste.py run get-data --cluster-id ${cluster_id} --run-id ${run_id}`;
+        
+        let runDirectory = `${cluster_id}\\${run_id}`
+        let inputFile  = `${runDirectory}\\stderr.txt`;
+        let outputFile = `${runDirectory}\\progress.json`;
+        let parseLogToJsonCommand = `cd .. && python cntklogparser.py ${inputFile} > ${outputFile}`
+        
+        exec(downloadLogCommand, (_err, _stdout, _stderr) => {
+            if (_err) {console.log(_err); return; }
+
+            exec(parseLogToJsonCommand, (__err, __stdout, __stderr) => {
+                if (__err) {console.log(__err); return; }
+
+                result = JSON.parse(require('fs').readFileSync(`..\\${cluster_id}\\${run_id}\\progress.json`, 'utf8'));
+                do_something_with_json(result);
+            })
         });
     }
 }
